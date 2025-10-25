544a17952d5b530b6704014acc313749d2cc157f
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-10-25 07:51:42 +0300

    Add admin products table with Edit and Delete links passing product ID

---
+++ admin/products.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once '../includes/db_connect.php';
+
+try {
+    $query = "SELECT * FROM products;";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Database error: " . htmlspecialchars($e->getMessage());
+    die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Products</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- css style -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Image</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+ 
+            <tbody>        
+        <?php
+
+
+        foreach ($products as $product) {
+        ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($product['id']); ?></td>
+                    <td><?php echo htmlspecialchars($product['name']); ?></td>
+                    <td><?= htmlspecialchars($product['description']); ?></td>
+                    <td><?= htmlspecialchars($product['price']); ?></td>
+                    <td><img src="<?= htmlspecialchars($product['image']); ?>" class="" alt="..." width="80"></td>
+                    <td>
+                        <a href="edit_product.php?id=<?php echo $product['id']; ?>">Edit</a>
+                        <a href="delete_product.php?id=<?php echo $product['id']; ?>">Delete</a>
+                    </td>
+                </tr>
+        <?php
+        }
+
+        ?>
+        
+            </tbody>
+
+    </table>
+
+</body>
+
+</html>
